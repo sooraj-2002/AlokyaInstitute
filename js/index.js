1dccd0be7ea27969d87af8b0502469452be0a1e2
Sooraj <86868761+sooraj-2002@users.noreply.github.com>
2023-05-21 16:47:47 +0530

    AlokyaInstitute

---
+++ js/index.js
@@ -0,0 +1,9 @@
+{/* <script type="text/javascript">
+        $(document).ready(function(){
+            setInterval(function(){
+                let h=$("#notice>ul>li").first().outerHeight();
+                $("#notice>ul").animate({"marginTop":"-"+h+"px"},1000,function(){$(this).append($("#notice>ul>li").first());$(this).css("margin-top","0")});
+               
+            },1000);
+        });
+    </script> */}
